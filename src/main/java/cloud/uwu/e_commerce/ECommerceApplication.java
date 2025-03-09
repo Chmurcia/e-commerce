b0906b11439d860cbcc03ea018085ac380a196a0
Igor Grabowski <igor.grabowski2006@protonmail.com>
2025-03-09 15:26:27 +0100

    Add ComponentScan

--- src/main/java/cloud/uwu/e_commerce/ECommerceApplication.java
+++ src/main/java/cloud/uwu/e_commerce/ECommerceApplication.java
@@ -2,8 +2,11 @@ package cloud.uwu.e_commerce;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.stereotype.Component;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"cloud.uwu.e_commerce", "cloud.uwu.e_commerce.repositories"})
 public class ECommerceApplication {
 
 	public static void main(String[] args) {
