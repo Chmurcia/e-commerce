5e4fe1de6ff2c116ab2e185e33fc2d59c5963b25
Igor Grabowski <igor.grabowski2006@protonmail.com>
2025-03-14 03:41:57 +0100

    add @Field to keep snake_case properties

--- src/main/java/cloud/uwu/e_commerce/model/cart/C_Item.java
+++ src/main/java/cloud/uwu/e_commerce/model/cart/C_Item.java
@@ -5,6 +5,7 @@ import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
 
 import java.time.LocalDateTime;
 
@@ -18,8 +19,10 @@ public class C_Item {
     @Id
     private String id;
 
+    @Field("cart_id")
     private String cartId;
 
+    @Field("product_id")
     private String productId;
 
     @CreatedDate
