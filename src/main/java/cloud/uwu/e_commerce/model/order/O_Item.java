8fe74d9d50aa7741cdcacb49434229bc92ca3b3c
Igor Grabowski <igor.grabowski2006@protonmail.com>
2025-03-14 04:33:01 +0100

    add document

--- src/main/java/cloud/uwu/e_commerce/model/order/O_Item.java
+++ src/main/java/cloud/uwu/e_commerce/model/order/O_Item.java
@@ -1,4 +1,33 @@
 package cloud.uwu.e_commerce.model.order;
 
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
+
+import java.time.LocalDateTime;
+
+@Document(collection = "o_item")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class O_Item {
+    @Id
+    private String id;
+
+    @Field("order_id")
+    private String orderId;
+
+    @Field("product_id")
+    private String productId;
+
+    @CreatedDate
+    private LocalDateTime createdDate;
+
+    @LastModifiedDate
+    private LocalDateTime updatedDate;
 }
--- src/main/java/cloud/uwu/e_commerce/model/order/Order.java
+++ src/main/java/cloud/uwu/e_commerce/model/order/Order.java
@@ -1,4 +1,39 @@
 package cloud.uwu.e_commerce.model.order;
 
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.mongodb.core.mapping.DBRef;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Document(collection = "order")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class Order {
+    @Id
+    private String id;
+
+    @Field("user_id")
+    private String userId;
+
+    @DBRef
+    private List<O_Item> items;
+
+    @Field("total_price")
+    private BigDecimal totalPrice;
+
+    @CreatedDate
+    private LocalDateTime createdDate;
+
+    @LastModifiedDate
+    private LocalDateTime updatedDate;
 }
