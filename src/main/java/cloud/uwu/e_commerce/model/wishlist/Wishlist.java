c62b14c7d4b662e417a41470fea63aa9296c7c27
Igor Grabowski <igor.grabowski2006@protonmail.com>
2025-03-14 04:31:36 +0100

    add @Field

--- src/main/java/cloud/uwu/e_commerce/model/wishlist/Wishlist.java
+++ src/main/java/cloud/uwu/e_commerce/model/wishlist/Wishlist.java
@@ -5,6 +5,7 @@ import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
@@ -19,6 +20,7 @@ public class Wishlist {
     @Id
     private String id;
 
+    @Field("user_id")
     private String userId;
 
     private Integer amount;
