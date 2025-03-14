68968d8532be880baf871f6e3cdead3b444fba01
Igor Grabowski <igor.grabowski2006@protonmail.com>
2025-03-14 03:48:37 +0100

    add document

---
+++ src/main/java/cloud/uwu/e_commerce/model/product/P_Settings.java
@@ -0,0 +1,38 @@
+package cloud.uwu.e_commerce.model.product;
+
+import cloud.uwu.e_commerce.enums.Region;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Document(collection = "p_settings")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class P_Settings {
+    @Id
+    private String id;
+
+    @Field("product_id")
+    private String productId;
+
+    @Field("is_public")
+    private Boolean isPublic;
+
+    @Field("available_regions")
+    private List<Region> availableRegions;
+
+    @CreatedDate
+    private LocalDateTime createdDate;
+
+    @LastModifiedDate
+    private LocalDateTime updatedDate;
+}
