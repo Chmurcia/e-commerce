796e9fc949f414fd0488e947939d4a8fdd6cf038
Igor Grabowski <igor.grabowski2006@protonmail.com>
2025-03-14 04:12:14 +0100

    add document

--- src/main/java/cloud/uwu/e_commerce/model/supplier/Supplier.java
+++ src/main/java/cloud/uwu/e_commerce/model/supplier/Supplier.java
@@ -1,4 +1,37 @@
 package cloud.uwu.e_commerce.model.supplier;
 
+import cloud.uwu.e_commerce.enums.Country;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.mongodb.core.mapping.Field;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class Supplier {
+    @Id
+    private String id;
+
+    private String name;
+
+    private String email;
+
+    @Field("phone_number")
+    private String phoneNumber;
+
+    @Field("operating_countries")
+    private List<Country> operatingCountries;
+
+    @CreatedDate
+    private LocalDateTime createdDate;
+
+    @LastModifiedDate
+    private LocalDateTime updatedDate;
 }
