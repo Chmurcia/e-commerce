e17493ecbb889f598a1cc7961b0cc6c518e1e2ee
Igor Grabowski <igor.grabowski2006@protonmail.com>
2025-03-14 04:32:05 +0100

    add @Document

--- src/main/java/cloud/uwu/e_commerce/model/warehouse/W_Address.java
+++ src/main/java/cloud/uwu/e_commerce/model/warehouse/W_Address.java
@@ -4,10 +4,12 @@ import lombok.*;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.Field;
 
 import java.time.LocalDateTime;
 
+@Document(collection = "w_address")
 @Getter
 @Setter
 @AllArgsConstructor
--- src/main/java/cloud/uwu/e_commerce/model/warehouse/W_Inventory.java
+++ src/main/java/cloud/uwu/e_commerce/model/warehouse/W_Inventory.java
@@ -5,11 +5,13 @@ import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.mongodb.core.mapping.DBRef;
+import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.Field;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
+@Document(collection = "w_inventory")
 @Getter
 @Setter
 @AllArgsConstructor
