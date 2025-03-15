71c451a1f6f48eeda3b961647e91411777c812aa
Igor Grabowski <igor.grabowski2006@protonmail.com>
2025-03-15 14:43:22 +0100

    change name

--- src/main/java/cloud/uwu/e_commerce/repositories/product/ProductRateRepository.java
+++ src/main/java/cloud/uwu/e_commerce/repositories/product/ProductRateRepository.java
@@ -3,5 +3,5 @@ package cloud.uwu.e_commerce.repositories.product;
 import cloud.uwu.e_commerce.model.product.P_Rate;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
 
-public interface RateRepository extends ReactiveMongoRepository<P_Rate, String> {
+public interface ProductRateRepository extends ReactiveMongoRepository<P_Rate, String> {
 }
