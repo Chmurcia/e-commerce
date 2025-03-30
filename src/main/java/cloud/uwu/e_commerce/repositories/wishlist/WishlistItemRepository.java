088cfeedc065ae772b444a3a5f3cc5d25b1b82c4
Igor Grabowski <igor.grabowski2006@protonmail.com>
2025-03-30 20:28:15 +0200

    change repositories

--- src/main/java/cloud/uwu/e_commerce/repositories/wishlist/WishlistItemRepository.java
+++ src/main/java/cloud/uwu/e_commerce/repositories/wishlist/WishlistItemRepository.java
@@ -2,6 +2,8 @@ package cloud.uwu.e_commerce.repositories.wishlist;
 
 import cloud.uwu.e_commerce.model.wishlist.W_Item;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+import reactor.core.publisher.Flux;
 
 public interface WishlistItemRepository extends ReactiveMongoRepository<W_Item, String> {
+    Flux<W_Item> findByWishlistId(String warehouseId);
 }
--- src/main/java/cloud/uwu/e_commerce/repositories/wishlist/WishlistRepository.java
+++ src/main/java/cloud/uwu/e_commerce/repositories/wishlist/WishlistRepository.java
@@ -2,6 +2,8 @@ package cloud.uwu.e_commerce.repositories.wishlist;
 
 import cloud.uwu.e_commerce.model.wishlist.Wishlist;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+import reactor.core.publisher.Mono;
 
 public interface WishlistRepository extends ReactiveMongoRepository<Wishlist, String> {
+    Mono<Wishlist> findByUserId(String userId);
 }
