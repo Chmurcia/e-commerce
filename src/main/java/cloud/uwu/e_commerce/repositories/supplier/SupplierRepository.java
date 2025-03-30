ad8acb6d082b9de457d0b160769b8edbb50a207c
Igor Grabowski <igor.grabowski2006@protonmail.com>
2025-03-30 21:48:05 +0200

    update SupplierRepository.java and UserRepository.java

--- src/main/java/cloud/uwu/e_commerce/repositories/supplier/SupplierRepository.java
+++ src/main/java/cloud/uwu/e_commerce/repositories/supplier/SupplierRepository.java
@@ -2,6 +2,10 @@ package cloud.uwu.e_commerce.repositories.supplier;
 
 import cloud.uwu.e_commerce.model.supplier.Supplier;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+import reactor.core.publisher.Mono;
 
 public interface SupplierRepository extends ReactiveMongoRepository<Supplier, String> {
+    Mono<Supplier> findByName(String name);
+    Mono<Supplier> findByEmail(String email);
+    Mono<Supplier> findByPhoneNumber(String phoneNumber);
 }
--- src/main/java/cloud/uwu/e_commerce/repositories/user/UserRepository.java
+++ src/main/java/cloud/uwu/e_commerce/repositories/user/UserRepository.java
@@ -2,6 +2,8 @@ package cloud.uwu.e_commerce.repositories.user;
 
 import cloud.uwu.e_commerce.model.user.User;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+import reactor.core.publisher.Mono;
 
 public interface UserRepository extends ReactiveMongoRepository<User, String> {
+    Mono<User> findByEmail(String email);
 }
