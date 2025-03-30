6e0b04ec3e108a3940db083ccd443a04b3976bd8
Igor Grabowski <igor.grabowski2006@protonmail.com>
2025-03-30 21:48:29 +0200

    update UserDTO.java

--- src/main/java/cloud/uwu/e_commerce/dto/user/user/UserDTO.java
+++ src/main/java/cloud/uwu/e_commerce/dto/user/user/UserDTO.java
@@ -2,10 +2,7 @@ package cloud.uwu.e_commerce.dto.user.user;
 
 import cloud.uwu.e_commerce.enums.Role;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.*;
 import lombok.*;
 
 @Getter
@@ -28,6 +25,11 @@ public class UserDTO {
     @Size(min = 1, max = 100, message = "length of 'last_name' field must be between 1 and 50")
     @JsonProperty("nick_name")
     private String nickName;
+
+    @NotBlank(message = "'phone_number' field is null")
+    @Pattern(regexp = "^(\\\\+\\\\d{1,3}\\\\s?)?((\\\\d{3}[- ]?){2}\\\\d{3})$")
+    @JsonProperty("phone_number")
+    private String phoneNumber;
 
     @NotBlank(message = "'email' field is null")
     @Email(message = "'email' field has invalid format")
